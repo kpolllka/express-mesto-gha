f6ede1f355702d57f2fdfeda446776ec490ea735
Антонова <sabrina-19@mail.ru>
2023-06-14 21:02:35 +0700

    Проект13 Вариант1

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const routerUser = require('express').Router();
 const {
   createUser, getUsers, getUserId, editUser, editAvatar,
 } = require('../controllers/users');
-// const { ERROR_NOT_FOUND } = require('../errors/errors');
-// const { MSG_ERROR_NOT_FOUND } = require('../errors/errors');
+const { ERROR_NOT_FOUND } = require('../errors/errors');
+const { MSG_ERROR_NOT_FOUND } = require('../errors/errors');
 
 routerUser.post('/users', createUser); // Создаём пользователя
 routerUser.get('/users', getUsers); // Получаем всех пользователей
@@ -11,6 +11,6 @@ routerUser.get('/users/:_id', getUserId); // Получаем пользоват
 routerUser.patch('/users/me', editUser); // Редактируем данные пользователя
 routerUser.patch('/users/me/avatar', editAvatar); // Редактируем аватар пользователя
 
-// routerUser.use((req, res) => res.status(ERROR_NOT_FOUND).send({ message: MSG_ERROR_NOT_FOUND }));
+routerUser.use((req, res) => res.status(ERROR_NOT_FOUND).send({ message: MSG_ERROR_NOT_FOUND }));
 
 module.exports = routerUser;
