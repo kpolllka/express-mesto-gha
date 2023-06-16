53d61cd41ec279fa9cda06755a89838d80675e4a
Антонова <sabrina-19@mail.ru>
2023-06-16 17:08:39 +0700

    Проект13 Вариант2

--- models/user.js
+++ models/user.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: true,
+    minlength: [2, 'Минимальная длина поля "name" - 2 символа'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30 символов'],
+    required: [true, 'Поле "name" должно быть заполнено'],
   },
   about: {
     type: String,
